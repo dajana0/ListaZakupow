45bab4f8bea79c0a6c486677e6603cb742068e6e
Dajana Kubica <dajana.kubica@outlook.com>
2017-11-11 23:09:58 +0100

    dodanie product item

--- app/src/main/java/dajana/listazakupow/ListActivity.java
+++ app/src/main/java/dajana/listazakupow/ListActivity.java
@@ -1,14 +1,22 @@
 package dajana.listazakupow;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class ListActivity extends AppCompatActivity {
-
+    final Context context =  this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,10 +26,59 @@ public class ListActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            final Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.product_dialog);
+                dialog.setTitle("Nowy produkt");
+
+                Button btAnuluj = (Button) dialog.findViewById(R.id.btAnuluj);
+                btAnuluj.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                final EditText nazwa = (EditText) dialog.findViewById(R.id.etNazwaProduktu);
+                nazwa.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(nazwa.getCurrentTextColor()==Color.RED){
+                            nazwa.setText("");
+                            SharedPreferences settings = getSharedPreferences("font_color", 0);
+                            nazwa.setTextColor(settings.getInt("color",Color.BLACK));
+                        }
+                    }
+                });
+                Button btZatwierdz = (Button) dialog.findViewById(R.id.btZatwierdz);
+                btZatwierdz.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(nazwa.getText()== null || nazwa.getText().length() == 0){
+                            nazwa.setTextColor(Color.RED);
+                            nazwa.setTypeface(null, Typeface.BOLD);
+                            nazwa.setText("Wpisz nazwę!!");
+                        }else{
+                            EditText ilosc = (EditText) dialog.findViewById(R.id.etIlosc);
+                            EditText cena = (EditText) dialog.findViewById(R.id.etCena);
+                            CheckBox kupiono = (CheckBox) dialog.findViewById(R.id.cbKupione);
+                            if(ilosc.getText().length() == 0){
+
+                            }else{
+
+                            }
+                            if(cena.getText().length() == 0){
+
+                            }else{
+
+                            }
+                        }
+                    }
+                });
+                dialog.show();
             }
         });
     }
 
+    private  void zbudujListe(){
+
+    }
 }
